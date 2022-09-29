b934bb00cf7df632e06896ff3a916b9a7038fe90
Guzel <muhametova-guzel@mail.ru>
2022-09-29 18:57:14 +0300

    home page

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/userReducer';
+import wishesReducer from './reducers/wishesReducer';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    wishes: wishesReducer,
   },
 });
--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -1,3 +1,6 @@
 // User
 export const SET_AUTH = 'SET_AUTH';
 export const LOGOUT = 'LOGOUT';
+
+// Wishes
+export const SET_WISHES = 'SET_WISHES';
