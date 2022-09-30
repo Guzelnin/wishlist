64c3277095edaf71bbde432fab2109ff42940d0e
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-09-30 10:10:39 +0300

    add card

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -3,14 +3,25 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-    >
-      HOME PAGE
-    </Box>
+    // <Box sx={{
+    //   display: 'flex',
+    //   flexDirection: 'row',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    // }}
+    // >
+    //   HOME PAGE
+    // </Box>
+    <div className="row">
+      {
+        // myWish && myWish?.map((el) => (
+        //   <MyWishCard
+        //     myWish={el}
+        //     key={el.id}
+        //     id={el.id}
+        //   />
+        // ))
+    }
+    </div>
   );
 }
