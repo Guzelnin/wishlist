a999affb5acb5f15a147943ed0b14a68bdf6d1ae
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-10-03 09:53:37 +0300

    decomposing

--- client/src/components/UserPage/UserPage.jsx
+++ client/src/components/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import { Container } from '@mui/system';
 import React from 'react';
-import PageNavbar from '../PageNavbar/PageNavbar';
+import PageNavbar from './PageNavbar';
 import UserDescription from '../UserDescription';
 
 import WishPlace from '../WishPlace';
