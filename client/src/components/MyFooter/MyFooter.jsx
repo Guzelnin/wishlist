533c6408feec8570a028d921c8b8738de0247f72
Yulia-Ya <yakushevayulia696@gmail.com>
2022-10-04 09:26:21 +0300

    cards style

--- client/src/components/MyFooter/MyFooter.jsx
+++ client/src/components/MyFooter/MyFooter.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
-import {
-  Box, Container, Typography, Paper, Grid,
-} from '@material-ui/core';
 import Social from './Social';
+
+const url = 'https://thumbs.dreamstime.com/b/литерность-вектора-руки-wishlist-вычерченная-145269082.jpg';
 
 export default function MyFooter() {
   return (
     <footer className="footer-distributed">
       <div className="footer-left">
+        <img src={url} alt="logo" className="logo" />
 
-        <h3>
-          Company
-          <span>logo</span>
-        </h3>
-
-        <p className="footer-links">
+        {/* <p className="footer-links">
           <a href="#" className="link-1">Home</a>
           <a href="#">Blog</a>
           <a href="#">Pricing</a>
           <a href="#">About</a>
           <a href="#">Faq</a>
           <a href="#">Contact</a>
-        </p>
+        </p> */}
 
-        <p className="footer-company-name">Company Name © 2015</p>
+        <p className="footer-company-name">ElbrusBootCamp © 2022</p>
       </div>
 
       <div className="footer-center">
@@ -31,20 +26,22 @@ export default function MyFooter() {
         <div>
           <i className="fa fa-map-marker" />
           <p>
-            <span>444 S. Cedros Ave</span>
+            <span>
+              ул.Орджоникидзе, д. 11 стр.10 
+            </span>
             {' '}
-            Solana Beach, California
+            Москва, Россия
           </p>
         </div>
 
         <div>
           <i className="fa fa-phone" />
-          <p>+1.555.555.5555</p>
+          <p>+7 499 938 68 24</p>
         </div>
 
         <div>
           <i className="fa fa-envelope" />
-          <p><a href="mailto:support@company.com">support@company.com</a></p>
+          <p><a href="mailto:support@company.com">info@elbrusboot.camp</a></p>
         </div>
 
       </div>
@@ -58,11 +55,11 @@ export default function MyFooter() {
 
         <div className="footer-icons">
 
-          <a href="#"><i className="fa fa-facebook" /></a>
+          {/* <a href="#"><i className="fa fa-facebook" /></a>
           <a href="#"><i className="fa fa-twitter" /></a>
           <a href="#"><i className="fa fa-linkedin" /></a>
-          <a href="#"><i className="fa fa-github" /></a>
-
+          <a href="#"><i className="fa fa-github" /></a> */}
+          <Social />
         </div>
 
       </div>
