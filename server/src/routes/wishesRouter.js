ba0ee79e60a75383658c9d5ccf713e7ca55124a8
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-10-06 10:17:08 +0300

    test

--- server/src/routes/wishesRouter.js
+++ server/src/routes/wishesRouter.js
@@ -54,6 +54,7 @@ router.get('/mypage/mywishes', async (req, res) => {
         where: { wish_status: true },
       }],
     });
+    console.log(myWishes);
     res.send(myWishes);
   } catch (error) {
     console.log(error);
